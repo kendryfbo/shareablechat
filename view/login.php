d8419502ecb8de75900bea5fd77bb4ef5b2d7918
Kendry <kendryfbo@gmail.com>
2017-01-19 19:18:36 -0300

    creation of login form

---
+++ view/login.php
@@ -0,0 +1,14 @@
+<br>
+  <div class="container">
+    <div class="card text-sm-center">
+      <h1 class="card-header bg-faded">Login</h1>
+      <form class="card-block" action="login.php" method="post">
+        <div class="form-group text-sm-center">
+          <label for="">User: </label>
+          <input class="form-control" type="text" name="user" value="">
+          <br>
+          <button class="btn btn-outline-success" type="submit" name="button">Login</button>
+        </div>
+      </form>
+    </div>
+  </div>
