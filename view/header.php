9dec4ecd0367afd7091bf7e9b2cb82c4aca6c501
Kendry <kendryfbo@gmail.com>
2017-01-19 19:12:55 -0300

    Creation of Header and navbar

---
+++ view/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Shareable Chat</title>
+  </head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+
+  <body>
+
+    <nav class="navbar navbar-toggleable-md navbar-light  bg-faded">
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="navbar-brand navbar" href="#">Shareable<b>Chat</b></a>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="nav navbar-nav">
+          <a class="nav-item nav-link active" href="index.php">Home<span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="login.php">Login</a>
+          <a class="nav-item nav-link disabled" href="#">Logout</a>
+        </div>
+      </div>
+    </nav>
